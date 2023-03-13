0c73c0a74ed6e5b42fcb051e63584bc0e539eb4b
Nikita Kuznetsov <nkuz915@gmail.com>
2023-03-13 22:18:42 +0100

    Rebuild

--- apps/web/src/libs/scroll.ts
+++ apps/web/src/libs/scroll.ts
@@ -28,13 +28,3 @@ export const getScrollbarWidth = () => {
 
   return scrollbarWidth;
 };
-
-
-export const virtualKeyboardHeight = () => {
-  var sx = document.body.scrollLeft, sy = document.body.scrollTop;
-  var naturalHeight = window.innerHeight;
-  window.scrollTo(sx, document.body.scrollHeight);
-  var keyboardHeight = naturalHeight - window.innerHeight;
-  window.scrollTo(sx, sy);
-  return keyboardHeight;
-};
