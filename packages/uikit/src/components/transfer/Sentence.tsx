8ee05d6488bce0bf44f7b55d90e1b22c6ed50575
Nikita Kuznetsov <nkuz915@gmail.com>
2023-03-20 23:17:47 +0100

    Add autofocus

--- packages/uikit/src/components/transfer/Sentence.tsx
+++ packages/uikit/src/components/transfer/Sentence.tsx
@@ -42,6 +42,7 @@ export const Sentence = React.forwardRef<HTMLInputElement, InputProps>(
   ({ value, setValue, inputSize }, ref) => {
     return (
       <SentenceInput
+        autoFocus
         ref={ref}
         style={{
           fontSize: `${inputSize.size}px`,
