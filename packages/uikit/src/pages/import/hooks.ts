29ced80ba4a92e773cc2d51d9c9261b53d27f169
Nikita Kuznetsov <nkuz915@gmail.com>
2023-05-04 19:56:47 +0200

    Update overflow

--- packages/uikit/src/pages/import/hooks.ts
+++ packages/uikit/src/pages/import/hooks.ts
@@ -15,8 +15,11 @@ export const useKeyboardHeight = () => {
     function resizeHandler(this: VisualViewport) {
       const doc = document.documentElement;
       doc.style.setProperty('--app-height', `${this.height}px`);
+      doc.style.overflow = 'hidden';
       doc.style.height = `${this.height}px`;
+      document.body.style.overflow = 'hidden';
       document.body.style.height = `${this.height}px`;
+      document.getElementById('root')!.style.overflow = 'hidden';
       document.getElementById('root')!.style.height = `${this.height}px`;
       message(`${this.height}px`);
     }
@@ -32,8 +35,11 @@ export const useKeyboardHeight = () => {
       doc.style.setProperty('--app-height', `${innerHeight}px`);
       message(`${innerHeight}px`);
 
+      delete doc.style.overflow;
       delete doc.style.height;
+      delete document.body.style.overflow;
       delete document.body.style.height;
+      delete document.getElementById('root')!.style.overflow;
       delete document.getElementById('root')!.style.height;
     };
   }, []);
