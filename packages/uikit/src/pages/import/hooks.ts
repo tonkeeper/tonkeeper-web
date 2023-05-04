e2759c0f937eed57ed8e9440f8b95858c59173c3
Nikita Kuznetsov <nkuz915@gmail.com>
2023-05-04 19:16:40 +0200

    Update reset viewport

--- packages/uikit/src/pages/import/hooks.ts
+++ packages/uikit/src/pages/import/hooks.ts
@@ -2,18 +2,11 @@ import { useEffect } from 'react';
 
 export const useKeyboardHeight = () => {
   useEffect(() => {
-    let height = window.innerHeight;
+    let innerHeight = window.innerHeight;
 
     function resizeHandler(this: VisualViewport) {
       const doc = document.documentElement;
       doc.style.setProperty('--app-height', `${this.height}px`);
-    }
-
-    function blurHandler() {
-      const viewport = window.visualViewport;
-      if (viewport) {
-        viewport.removeEventListener('resize', resizeHandler);
-      }
     }
 
     const viewport = window.visualViewport;
@@ -23,7 +16,8 @@ export const useKeyboardHeight = () => {
     }
 
     return () => {
-      blurHandler();
+      const doc = document.documentElement;
+      doc.style.setProperty('--app-height', `${innerHeight}px`);
     };
   }, []);
 };
