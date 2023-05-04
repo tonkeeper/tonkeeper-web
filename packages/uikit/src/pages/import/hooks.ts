b19a6e569666b94d29b216ffb153d817bdb6de8d
Nikita Kuznetsov <nkuz915@gmail.com>
2023-05-04 19:18:32 +0200

    Height message

--- packages/uikit/src/pages/import/hooks.ts
+++ packages/uikit/src/pages/import/hooks.ts
@@ -1,12 +1,19 @@
 import { useEffect } from 'react';
+import { useAppSdk } from '../../hooks/appSdk';
 
 export const useKeyboardHeight = () => {
+  const sdk = useAppSdk();
+
   useEffect(() => {
+    const message = (value: string) =>
+      sdk.uiEvents.emit('copy', { method: 'copy', params: value });
+
     let innerHeight = window.innerHeight;
 
     function resizeHandler(this: VisualViewport) {
       const doc = document.documentElement;
       doc.style.setProperty('--app-height', `${this.height}px`);
+      message(`${this.height}px`);
     }
 
     const viewport = window.visualViewport;
@@ -18,6 +25,7 @@ export const useKeyboardHeight = () => {
     return () => {
       const doc = document.documentElement;
       doc.style.setProperty('--app-height', `${innerHeight}px`);
+      message(`${innerHeight}px`);
     };
   }, []);
 };
