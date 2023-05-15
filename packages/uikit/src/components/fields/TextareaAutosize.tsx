78ac3ddb08ce16efeb8937e0a712210e9d80e40b
Nikita Kuznetsov <nkuz915@gmail.com>
2023-05-15 16:33:47 +0200

    log input size

--- packages/uikit/src/components/fields/TextareaAutosize.tsx
+++ packages/uikit/src/components/fields/TextareaAutosize.tsx
@@ -2,6 +2,7 @@ import { debounce } from '@tonkeeper/core/dist/utils/common';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import styled from 'styled-components';
+import { useAppSdk } from '../../hooks/appSdk';
 
 const useEnhancedEffect =
   typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect;
@@ -151,6 +152,8 @@ export const TextareaAutosize = React.forwardRef(function TextareaAutosize(
   const [state, setState] = React.useState<State>({
     outerHeightStyle: 0,
   });
+
+  const sdk = useAppSdk();
 
   const getUpdatedState = React.useCallback(() => {
     const input = inputRef.current!;
@@ -319,6 +322,13 @@ export const TextareaAutosize = React.forwardRef(function TextareaAutosize(
       onChange(event);
     }
   };
+
+  React.useEffect(() => {
+    sdk.uiEvents.emit('copy', {
+      method: 'copy',
+      params: String(state.outerHeightStyle),
+    });
+  }, [sdk, state.outerHeightStyle]);
 
   return (
     <React.Fragment>
