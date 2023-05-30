6b90eefb43b7b3269817e08902af22be080d5e4a
Nikita Kuznetsov <nkuz915@gmail.com>
2023-05-30 19:29:28 +0200

    Add LanguageDetector

--- apps/web/src/i18n.ts
+++ apps/web/src/i18n.ts
@@ -1,10 +1,12 @@
 import resources from '@tonkeeper/locales/dist/i18n/default.json';
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
 i18n
   .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
