8797849d4b8443262b970ebd77822b9d42ed0728
Nikita Kuznetsov <nkuznetsov@ebay.com>
2023-06-07 14:01:22 +0200

    Update build

--- apps/extension/task/build.ts
+++ apps/extension/task/build.ts
@@ -1,6 +1,5 @@
-import { execSync } from 'child_process';
+import child_process, { execSync } from 'child_process';
 import fs from 'fs-extra';
-import zl from 'zip-lib';
 
 const notify = (value: string) => console.log(`----------${value}----------`);
 
@@ -20,73 +19,73 @@ const exec = (cmd: string, options: Record<string, any>) => {
   }
 };
 
-(async () => {
-  notify('Build UI');
+notify('Build UI');
 
-  exec('npx react-app-rewired build', {
-    stdio: 'inherit',
-    env: {
-      ...process.env,
-      INLINE_RUNTIME_CHUNK: false,
-    },
-  });
+exec('npx react-app-rewired build', {
+  stdio: 'inherit',
+  env: {
+    ...process.env,
+    INLINE_RUNTIME_CHUNK: false,
+  },
+});
 
-  notify(`Build Tonkeeper background.js, provider.js, content.js`);
+notify(`Build Tonkeeper background.js, provider.js, content.js`);
 
-  exec('npx webpack -c ./task/webpack.config.js', {
-    stdio: 'inherit',
-    env: process.env,
-  });
+exec('npx webpack -c ./task/webpack.config.js', {
+  stdio: 'inherit',
+  env: process.env,
+});
 
-  notify('Copy Locales');
+notify('Copy Locales');
 
-  const srcDir = `../../packages/locales/dist/extension`;
-  const destDir = `build/_locales`;
-  fs.copySync(srcDir, destDir, { overwrite: true });
+const srcDir = `../../packages/locales/dist/extension`;
+const destDir = `build/_locales`;
+fs.copySync(srcDir, destDir, { overwrite: true });
 
-  notify(`Create Chrome Build`);
+notify(`Create Chrome Build`);
 
-  const buildDir = `build`;
-  const buildDirChrome = `dist/chrome`;
-  const buildDirFireFox = `dist/firefox`;
+const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
-  fs.rmSync('dist', { recursive: true, force: true });
-  fs.mkdirSync('dist');
-  fs.mkdirSync(buildDirChrome);
-  fs.mkdirSync(buildDirFireFox);
+const buildDir = `build`;
+const buildDirChrome = `dist/chrome`;
+const buildDirFireFox = `dist/firefox`;
 
-  const manifestChrome = `${buildDirChrome}/manifest.json`;
-  const manifestChromeData = JSON.parse(
-    fs.readFileSync(manifestChrome, 'utf8')
-  );
+fs.rmSync('dist', { recursive: true, force: true });
+fs.mkdirSync('dist');
+fs.mkdirSync(buildDirChrome);
+fs.mkdirSync(buildDirFireFox);
 
-  fs.copySync(buildDir, buildDirChrome, { overwrite: true });
+fs.copySync(buildDir, buildDirChrome);
 
-  await zl.archiveFolder(
-    buildDirChrome,
-    `${buildDirChrome}/tonkeeper_chrome_${manifestChromeData.version}.zip`
-  );
+const manifestChrome = `${buildDirChrome}/manifest.json`;
+const manifestChromeData = JSON.parse(fs.readFileSync(manifestChrome, 'utf8'));
+manifestChromeData.version = packageJson.version;
+fs.writeFileSync(manifestChrome, JSON.stringify(manifestChromeData));
 
-  notify(`Create FireFox Build`);
+child_process.execSync(
+  `zip tonkeeper_chrome_${manifestChromeData.version}.zip -r ${buildDirChrome}/ *`,
+  {
+    cwd: buildDirChrome + '/',
+  }
+);
 
-  fs.copySync(buildDir, buildDirFireFox, { overwrite: true });
+notify(`Create FireFox Build`);
 
-  const manifest = `${buildDirFireFox}/manifest.json`;
-  const manifestFFData = JSON.parse(fs.readFileSync(manifest, 'utf8'));
-  manifestFFData.background = {
-    scripts: ['background.js'],
-  };
-  fs.writeFileSync(manifest, JSON.stringify(manifestFFData));
+fs.copySync(buildDir, buildDirFireFox);
 
-  await zl.archiveFolder(
-    buildDirFireFox,
-    `${buildDirFireFox}/tonkeeper_firefox_${manifestFFData.version}.zip`
-  );
+const manifest = `${buildDirFireFox}/manifest.json`;
+const manifestFFData = JSON.parse(fs.readFileSync(manifest, 'utf8'));
+manifestFFData.version = packageJson.version;
+manifestFFData.background = {
+  scripts: ['background.js'],
+};
+fs.writeFileSync(manifest, JSON.stringify(manifestFFData));
 
-  notify('End Build Extension');
-})()
-  .then(process.exit(0))
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
+child_process.execSync(
+  `zip tonkeeper_firefox_${manifestChromeData.version}.zip -r ${buildDirFireFox}/ *`,
+  {
+    cwd: buildDirFireFox + '/',
+  }
+);
+
+notify('End Build Extension');
