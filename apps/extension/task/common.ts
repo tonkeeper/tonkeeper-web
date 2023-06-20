8bbc8a046ece7128da431532a4c168dc3a1f0ce5
Nikita Kuznetsov <nkuz915@gmail.com>
2023-06-20 17:04:17 +0200

    Update build and create beta builds

--- apps/extension/task/common.ts
+++ apps/extension/task/common.ts
@@ -44,10 +44,36 @@ const addEnvironmentVariable = (path: string, key: string, value: string) => {
   fs.writeFileSync(`${path}${pathToMain}`, main);
 };
 
+const getDirectories = (source: string) =>
+  fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+
+const updateApplicationName = (
+  path: string,
+  appName: string,
+  appExtensionDescription: string
+) => {
+  const langs = getDirectories(`${path}/_locales`);
+
+  for (let lang of langs) {
+    const messagesPath = `${path}/_locales/${lang}/messages.json`;
+
+    const messages = JSON.parse(fs.readFileSync(messagesPath, 'utf8'));
+
+    messages.appName.message = appName;
+    messages.appExtensionDescription.message = appExtensionDescription;
+
+    fs.writeFileSync(messagesPath, JSON.stringify(messages));
+  }
+};
+
 export default {
   notify,
   exec,
   readManifest,
   writeManifest,
   addEnvironmentVariable,
+  updateApplicationName,
 };
