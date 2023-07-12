347678a3b2df17da9bbb7d558de4aa02ee1987cf
Nikita Kuznetsov <nkuznetsov@ebay.com>
2023-07-12 15:40:07 +0200

    dev ton api

--- packages/core/src/entries/network.ts
+++ packages/core/src/entries/network.ts
@@ -17,7 +17,7 @@ export const getTonClient = (config: TonendpointConfig, current?: Network) => {
   return new Configuration({
     basePath:
       current === Network.MAINNET
-        ? 'https://tonapi.io'
+        ? 'https://dev.tonapi.io'
         : 'https://testnet.tonapi.io',
     headers: {
       Authorization: `Bearer ${config.tonApiKey}`,
