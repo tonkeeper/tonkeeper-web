79a0070db40c0cd6c4f5c085d94c519e441cea69
Nikita Kuznetsov <nkuz915@gmail.com>
2023-09-07 17:41:27 +0200

    Add twa application

---
+++ apps/twa/config-overrides.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+    config.resolve.fallback = {
+        ...config.resolve.fallback,
+        buffer: require.resolve('buffer')
+    };
+    config.resolve.extensions = [...config.resolve.extensions, '.ts', '.js'];
+    config.plugins = [
+        ...config.plugins,
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer']
+        })
+    ];
+
+    return config;
+};
