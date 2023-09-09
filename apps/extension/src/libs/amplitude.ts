45a06945c8f49874c7cb96f461d6a16491aa081f
Nikita Kuznetsov <nkuz915@gmail.com>
2023-09-09 03:17:03 +0200

    Update version and extension

--- apps/extension/src/libs/amplitude.ts
+++ apps/extension/src/libs/amplitude.ts
@@ -1,19 +1,26 @@
 import * as amplitude from '@amplitude/analytics-browser';
 import { AmplitudeAnalyticsContext } from '@tonkeeper/uikit/dist/hooks/amplitude';
 import { useContext, useEffect } from 'react';
+import ReactGA from 'react-ga4';
 import { NotificationData } from './event';
 
-export const useNotificationAnalytics = (
-  item: NotificationData | undefined
-) => {
-  const enable = useContext(AmplitudeAnalyticsContext);
+export const useNotificationAnalytics = (item: NotificationData | undefined) => {
+    const enable = useContext(AmplitudeAnalyticsContext);
 
-  useEffect(() => {
-    if (enable === true && item != null) {
-      amplitude.track('Notification', {
-        name: item.kind,
-        origin: item.origin,
-      });
-    }
-  }, [enable, item]);
+    useEffect(() => {
+        if (enable && item != null) {
+            if (enable[0] === true) {
+                ReactGA.event('Notification', {
+                    name: item.kind,
+                    origin: item.origin
+                });
+            }
+            if (enable[1] === true) {
+                amplitude.track('Notification', {
+                    name: item.kind,
+                    origin: item.origin
+                });
+            }
+        }
+    }, [enable, item]);
 };
