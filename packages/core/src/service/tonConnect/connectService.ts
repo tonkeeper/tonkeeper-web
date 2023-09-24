a198aa2ba91fcdfc9d2a1312cc480215c74032b9
Nikita Kuznetsov <nkuz915@gmail.com>
2023-09-24 18:48:46 +0200

    Get manifest with cors

--- packages/core/src/service/tonConnect/connectService.ts
+++ packages/core/src/service/tonConnect/connectService.ts
@@ -95,9 +95,20 @@ export const getTonConnectParams = async (
     };
 };
 
+const getManifestResponse = async (manifestUrl: string) => {
+    try {
+        return await fetch(manifestUrl);
+    } catch (e) {
+        /**
+         * Request file with CORS header;
+         */
+        return await fetch(`https://cors-anywhere.herokuapp.com/${manifestUrl}`);
+    }
+};
+
 export const getManifest = async (request: ConnectRequest) => {
     // TODO: get fetch from context
-    const response = await fetch(request.manifestUrl);
+    const response = await getManifestResponse(request.manifestUrl);
 
     if (response.status != 200) {
         throw new Error('Failed to load Manifest');
