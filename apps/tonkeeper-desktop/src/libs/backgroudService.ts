ea1d65caad5a1838b383e8d82dd79331e8a70002
Nikita Kuznetsov <nkuz915@gmail.com>
2023-12-08 16:43:54 +0100

    Update packages

--- apps/tonkeeper-desktop/src/libs/backgroudService.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from './message';
-
-export const sendBackground = async <Result>(message: Message) => {
-  const response = await window.backgroundApi.message(message);
-  if (response instanceof Error) {
-    throw response;
-  }
-  return response as Result;
-};
