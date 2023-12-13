947c842208ef409a25265f46310448c5a963ea47
Nikita Kuznetsov <nkuz915@gmail.com>
2023-12-13 11:27:04 +0100

    Sign dmg

--- apps/desktop/forge.config.ts
+++ apps/desktop/forge.config.ts
@@ -1,10 +1,14 @@
+require('dotenv').config();
+
 import { MakerDeb } from '@electron-forge/maker-deb';
+import { MakerDMG } from '@electron-forge/maker-dmg';
 import { MakerRpm } from '@electron-forge/maker-rpm';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import type { ForgeConfig } from '@electron-forge/shared-types';
+import type { NotaryToolCredentials } from '@electron/notarize/lib/types';
 
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
@@ -13,12 +17,20 @@ const config: ForgeConfig = {
     packagerConfig: {
         asar: true,
         icon: './public/icon',
-        name: 'Tonkeeper'
+        name: 'Tonkeeper Pro',
+        appBundleId: process.env.APPLE_BUILD_ID,
+        osxSign: {},
+        osxNotarize: {
+            appleId: process.env.APPLE_ID,
+            appleIdPassword: process.env.APPLE_ID_PASSWORD,
+            teamId: process.env.APPLE_TEAM_ID
+        } as NotaryToolCredentials
     },
     rebuildConfig: {},
     makers: [
         new MakerSquirrel({}),
         new MakerZIP({}, ['darwin']),
+        new MakerDMG({}, ['darwin']),
         new MakerRpm({}),
         new MakerDeb({})
     ],
