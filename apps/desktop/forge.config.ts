7bd41e5466e53b170dcbd6c995a43cd17dc2c4cd
Nikita Kuznetsov <nkuz915@gmail.com>
2024-01-11 16:38:53 +0100

    Add loading gif

--- apps/desktop/forge.config.ts
+++ apps/desktop/forge.config.ts
@@ -67,6 +67,7 @@ const config: ForgeConfig = {
                 description: 'Your desktop wallet on The Open Network',
                 iconUrl: 'https://tonkeeper.com/assets/icon.ico',
                 setupIcon: path.join(process.cwd(), 'public', 'icon.ico'),
+                loadingGif: path.join(process.cwd(), 'public', 'install.gif'),
                 remoteReleases:
                     'https://github.com/tonkeeper/tonkeeper-web/releases/latest/download'
             },
