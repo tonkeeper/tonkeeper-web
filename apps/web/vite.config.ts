541e62a5b0456b9483b13d9bfb7f4e6741e961d4
Nikita Kuznetsov <nkuz915@gmail.com>
2024-01-12 19:06:26 +0100

    WIP

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,6 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-    plugins: [nodePolyfills(), react()]
+    plugins: [react()]
 });
