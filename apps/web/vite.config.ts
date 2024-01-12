b080b52f34630ec9912c304cd16ab95395b18366
Nikita Kuznetsov <nkuz915@gmail.com>
2024-01-12 18:33:13 +0100

    Downgrade vite

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,7 @@
 import react from '@vitejs/plugin-react';
-import rollupNodePolyfills from 'rollup-plugin-polyfill-node';
 import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
-export default defineConfig(({ mode }) => ({
-    plugins: mode === 'development' ? [nodePolyfills(), react()] : [],
-    build: {
-        rollupOptions: {
-            plugins: [rollupNodePolyfills()]
-        }
-    }
-}));
+export default defineConfig({
+    plugins: [nodePolyfills(), react()]
+});
