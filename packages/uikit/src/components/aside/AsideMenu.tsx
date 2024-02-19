369b10a6ac115baadf08f16645495dc59962bc34
siandreev <andreev.sergey.i@yandex.ru>
2024-02-19 17:23:40 +0100

    fix: aside menu items added

--- packages/uikit/src/components/aside/AsideMenu.tsx
+++ packages/uikit/src/components/aside/AsideMenu.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useMemo } from 'react';
+import React, { FC, useCallback, useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { useWalletState } from '../../state/wallet';
 import { Body2, Body3 } from '../Text';
@@ -7,12 +7,31 @@ import { useAppContext } from '../../hooks/appContext';
 import { useTranslation } from '../../hooks/translation';
 import { useMutateActiveWallet } from '../../state/account';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { AppProRoute, AppRoute } from '../../libs/routes';
+import { AppProRoute, AppRoute, SettingsRoute } from '../../libs/routes';
 import { scrollToTop } from '../../libs/common';
+import { ImportNotification } from '../create/ImportNotification';
+import { PlusIcon, SlidersIcon } from '../Icon';
 
 const AsideContainer = styled.div`
     background: ${p => p.theme.backgroundContent};
+    position: relative;
+    overflow: auto;
+    display: flex;
+    flex-direction: column;
+`;
+
+const AsideScrollContent = styled.div`
     padding: 0.5rem 0;
+    flex: 1;
+`;
+
+const IconWrapper = styled.div`
+    color: ${p => p.theme.iconSecondary};
+    height: fit-content;
+
+    > svg {
+        display: block;
+    }
 `;
 
 const AsideMenuCard = styled.button<{ isSelected: boolean; padding: 's' | 'm' }>`
@@ -32,6 +51,20 @@ const AsideMenuCard = styled.button<{ isSelected: boolean; padding: 's' | 'm' }>
     &:hover {
         background: ${p => p.theme.backgroundContentTint};
     }
+`;
+
+const AsideMenuBottom = styled.div`
+    position: sticky;
+    bottom: 0;
+    padding-bottom: 0.5rem;
+    background: inherit;
+`;
+
+const AsideMenuItemIcon = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
 `;
 
 export const AsideMenuAccount: FC<{ publicKey: string; isSelected: boolean }> = ({
@@ -72,20 +105,25 @@ export const AsideMenuAccount: FC<{ publicKey: string; isSelected: boolean }> = 
 };
 
 export const AsideMenu: FC<{ className?: string }> = ({ className }) => {
+    const [isOpenImport, setIsOpenImport] = useState(false);
     const { account } = useAppContext();
     const navigate = useNavigate();
     const location = useLocation();
 
-    const activeProRoute = useMemo<AppProRoute | undefined>(() => {
+    const activeRoute = useMemo<string | undefined>(() => {
         if (location.pathname.startsWith(AppProRoute.dashboard)) {
             return AppProRoute.dashboard;
+        }
+
+        if (location.pathname.startsWith(AppRoute.settings + SettingsRoute.account)) {
+            return AppRoute.settings + SettingsRoute.account;
         }
 
         return undefined;
     }, [location.pathname]);
 
     const handleNavigateClick = useCallback(
-        (route: AppProRoute) => {
+        (route: string) => {
             if (location.pathname !== route) {
                 return navigate(route);
             } else {
@@ -97,24 +135,49 @@ export const AsideMenu: FC<{ className?: string }> = ({ className }) => {
 
     return (
         <AsideContainer className={className}>
-            <AsideMenuCard
-                isSelected={activeProRoute === AppProRoute.dashboard}
-                padding="m"
-                onClick={() => handleNavigateClick(AppProRoute.dashboard)}
-            >
-                <Body2>Dashboard</Body2> {/*TODO*/}
-            </AsideMenuCard>
-            {account.publicKeys.map(publicKey => (
-                <AsideMenuAccount
-                    key={publicKey}
-                    publicKey={publicKey}
-                    isSelected={
-                        !activeProRoute &&
-                        !!account.activePublicKey &&
-                        account.activePublicKey === publicKey
-                    }
-                />
-            ))}
+            <AsideScrollContent>
+                <AsideMenuCard
+                    isSelected={activeRoute === AppProRoute.dashboard}
+                    padding="m"
+                    onClick={() => handleNavigateClick(AppProRoute.dashboard)}
+                >
+                    <Body2>Dashboard</Body2> {/*TODO*/}
+                </AsideMenuCard>
+                {account.publicKeys.map(publicKey => (
+                    <AsideMenuAccount
+                        key={publicKey}
+                        publicKey={publicKey}
+                        isSelected={
+                            !activeRoute &&
+                            !!account.activePublicKey &&
+                            account.activePublicKey === publicKey
+                        }
+                    />
+                ))}
+            </AsideScrollContent>
+            <AsideMenuBottom>
+                <AsideMenuCard padding="m" isSelected={false} onClick={() => setIsOpenImport(true)}>
+                    <AsideMenuItemIcon>
+                        <Body2>Add Wallet</Body2>
+                        <IconWrapper>
+                            <PlusIcon />
+                        </IconWrapper>
+                    </AsideMenuItemIcon>
+                </AsideMenuCard>
+                <AsideMenuCard
+                    padding="m"
+                    onClick={() => handleNavigateClick(AppRoute.settings + SettingsRoute.account)}
+                    isSelected={activeRoute === AppRoute.settings + SettingsRoute.account}
+                >
+                    <AsideMenuItemIcon>
+                        <Body2>Settings</Body2>
+                        <IconWrapper>
+                            <SlidersIcon />
+                        </IconWrapper>
+                    </AsideMenuItemIcon>
+                </AsideMenuCard>
+            </AsideMenuBottom>
+            <ImportNotification isOpen={isOpenImport} setOpen={setIsOpenImport} />
         </AsideContainer>
     );
 };
