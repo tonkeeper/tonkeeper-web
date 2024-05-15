b50d0a64b531ba1e10545f22ba38cbcaf43195d6
Nikita Kuznetsov <nkuz915@gmail.com>
2024-05-15 15:16:23 +0200

    Burn notcoin 2.0

--- packages/uikit/src/desktop-pages/notcoin/NotcoinPage.tsx
+++ packages/uikit/src/desktop-pages/notcoin/NotcoinPage.tsx
@@ -1,9 +1,21 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { Address, BitString, SendMode, beginCell, internal, toNano } from '@ton/core';
 import { APIConfig } from '@tonkeeper/core/dist/entries/apis';
 import { CellSigner, Signer } from '@tonkeeper/core/dist/entries/signer';
-import { getWalletSeqNo } from '@tonkeeper/core/dist/service/transfer/common';
-import { sendNftMultiTransfer } from '@tonkeeper/core/dist/service/transfer/multiSendService';
-import { AccountsApi, NftItem } from '@tonkeeper/core/dist/tonApiV2';
+import { WalletState } from '@tonkeeper/core/dist/entries/wallet';
+import {
+    checkWalletBalanceOrDie,
+    externalMessage,
+    getServerTime,
+    getTTL,
+    getTonkeeperQueryId,
+    getWalletBalance,
+    getWalletSeqNo,
+    signEstimateMessage
+} from '@tonkeeper/core/dist/service/transfer/common';
+import { walletContractFromState } from '@tonkeeper/core/dist/service/wallet/contractService';
+import { AccountsApi, BlockchainApi, EmulationApi, NftItem } from '@tonkeeper/core/dist/tonApiV2';
+import { TonendpointConfig } from '@tonkeeper/core/dist/tonkeeperApi/tonendpoint';
 import { unShiftedDecimals } from '@tonkeeper/core/dist/utils/balance';
 import { delay } from '@tonkeeper/core/dist/utils/common';
 import { FC, useEffect, useRef, useState } from 'react';
@@ -19,6 +31,7 @@ import {
 import { Button } from '../../components/fields/Button';
 import { useAppContext, useWalletContext } from '../../hooks/appContext';
 import { useAppSdk, useToast } from '../../hooks/appSdk';
+import { useDateTimeFormat } from '../../hooks/useDateTimeFormat';
 import { getSigner } from '../../state/mnemonic';
 import { useCheckTouchId } from '../../state/password';
 
@@ -31,7 +44,7 @@ const useVouchers = () => {
     const getItems = async (offset: number) => {
         const items = await new AccountsApi(api.tonApiV2).getAccountNftItems({
             accountId: wallet.active.rawAddress,
-            collection: 'EQDmkj65Ab_m0aZaW8IpKw4kYqIgITw_HRstYEkVQ6NIYCyW',
+            //   collection: 'EQDmkj65Ab_m0aZaW8IpKw4kYqIgITw_HRstYEkVQ6NIYCyW',
             limit: limit,
             offset: offset
         });
@@ -71,7 +84,115 @@ export const confirmWalletSeqNo = async (
     } while (walletSeqNo <= currentSeqNo);
 };
 
-const useBurnMutation = () => {
+const getNotcoinBurnAddress = (nftAddress: string, config: TonendpointConfig) => {
+    const nftAddressBits = new BitString(Address.parse(nftAddress).hash, 0, 4);
+
+    const burnAddresses = config.notcoin_burn_addresses ?? [];
+    const burnAddressesBits = burnAddresses.map(
+        (address: string) => new BitString(Address.parse(address).hash, 0, 4)
+    );
+
+    const index = burnAddressesBits.findIndex(item => nftAddressBits.equals(item));
+
+    return Address.parse(burnAddresses[index]);
+};
+
+const checkBurnDate = async (api: APIConfig, config: TonendpointConfig) => {
+    const burnTimestamp = config.notcoin_burn_date
+        ? config.notcoin_burn_date
+        : Date.now() / 1000 + 300;
+    const nowTimestamp = await getServerTime(api);
+
+    if (burnTimestamp > nowTimestamp) {
+        return [false, burnTimestamp] as const;
+    }
+
+    return [true, burnTimestamp] as const;
+};
+
+const createNftMultiTransfer = async (
+    timestamp: number,
+    seqno: number,
+    walletState: WalletState,
+    chunk: NftItem[],
+    config: TonendpointConfig,
+    signer: CellSigner
+) => {
+    const contract = walletContractFromState(walletState);
+
+    const transfer = await contract.createTransferAndSignRequestAsync({
+        seqno,
+        signer,
+        timeout: getTTL(timestamp),
+        sendMode: SendMode.PAY_GAS_SEPARATELY + SendMode.IGNORE_ERRORS,
+        messages: chunk.map(nft => {
+            return internal({
+                to: Address.parse(nft.address),
+                bounce: true,
+                value: toNano('0.07'),
+                body: beginCell()
+                    .storeUint(0x5fcc3d14, 32) // transfer op
+                    .storeUint(getTonkeeperQueryId(), 64)
+                    .storeAddress(getNotcoinBurnAddress(nft.address, config))
+                    .storeAddress(Address.parse(walletState.active.rawAddress))
+                    .storeBit(false)
+                    .storeCoins(toNano('0.05'))
+                    .storeBit(false)
+                    .storeUint(0x5fec6642, 32)
+                    .storeUint(nft.index, 64)
+                    .endCell()
+            });
+        })
+    });
+
+    return externalMessage(contract, seqno, transfer).toBoc();
+};
+
+const sendNftMultiTransfer = async (
+    api: APIConfig,
+    walletState: WalletState,
+    chunk: NftItem[],
+    config: TonendpointConfig,
+    signer: CellSigner
+) => {
+    const timestamp = await getServerTime(api);
+
+    const [wallet, seqno] = await getWalletBalance(api, walletState);
+
+    checkWalletBalanceOrDie(unShiftedDecimals(0.07).multipliedBy(chunk.length), wallet);
+
+    const estimationCell = await createNftMultiTransfer(
+        timestamp,
+        seqno,
+        walletState,
+        chunk,
+        config,
+        signEstimateMessage
+    );
+
+    const res = await new EmulationApi(api.tonApiV2).emulateMessageToAccountEvent({
+        ignoreSignatureCheck: true,
+        accountId: wallet.address,
+        decodeMessageRequest: { boc: estimationCell.toString('base64') }
+    });
+
+    if (
+        res.actions
+            .filter(action => action.type === 'NftItemTransfer')
+            .some(action => action.status !== 'ok')
+    ) {
+        throw new Error('NFT transfer estimation failed');
+    }
+
+    const cell = await createNftMultiTransfer(timestamp, seqno, walletState, chunk, config, signer);
+
+    await new BlockchainApi(api.tonApiV2).sendBlockchainMessage({
+        sendBlockchainMessageRequest: { boc: cell.toString('base64') }
+    });
+    return true;
+};
+
+const useBurnMutation = (config: TonendpointConfig) => {
     const wallet = useWalletContext();
     const { api } = useAppContext();
 
@@ -83,19 +204,9 @@ const useBurnMutation = () => {
 
             const seqno = await getWalletSeqNo(api, wallet.active.rawAddress);
 
-            const transferMessages = chunk.map(item => ({
-                nft: item.address,
-                to: 'EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c'
-            }));
-            console.log('send', transferMessages);
+            console.log('send', chunk);
 
-            await sendNftMultiTransfer(
-                api,
-                wallet,
-                transferMessages,
-                unShiftedDecimals(0.04),
-                signer as CellSigner
-            );
+            await sendNftMultiTransfer(api, wallet, chunk, config, signer as CellSigner);
 
             await confirmWalletSeqNo(wallet.active.rawAddress, api, seqno);
 
@@ -151,11 +262,13 @@ const BodyCenter = styled(Body2)`
 `;
 
 const BurnBlock: FC<{ data: NftItem[] | undefined }> = ({ data }) => {
+    const { api, config } = useAppContext();
+
     const [burning, setBurning] = useState(false);
     const [ok, setIsOk] = useState(false);
     const [left, setLeft] = useState(0);
 
-    const mutation = useBurnMutation();
+    const mutation = useBurnMutation(config);
     const toast = useToast();
 
     const { mutateAsync: checkTouchId } = useCheckTouchId();
@@ -164,6 +277,7 @@ const BurnBlock: FC<{ data: NftItem[] | undefined }> = ({ data }) => {
     const process = useRef(true);
     const sdk = useAppSdk();
     const client = useQueryClient();
+    const formatDate = useDateTimeFormat();
 
     useEffect(() => {
         return () => {
@@ -171,7 +285,21 @@ const BurnBlock: FC<{ data: NftItem[] | undefined }> = ({ data }) => {
             client.invalidateQueries(['notcoin']);
         };
     }, []);
+
     const onBurn = async () => {
+        const [allow, time] = await checkBurnDate(api, config);
+        if (!allow) {
+            toast(
+                `Burning NOT Vouchers will be available soon! ${formatDate(new Date(time * 1000), {
+                    day: 'numeric',
+                    month: 'short',
+                    year: 'numeric',
+                    inputUnit: 'seconds'
+                })}`
+            );
+            return;
+        }
+
         setLeft(data?.length ?? 0);
         setBurning(true);
 
@@ -205,16 +333,15 @@ const BurnBlock: FC<{ data: NftItem[] | undefined }> = ({ data }) => {
                 <NotCoinIcon size="128" />
                 <Center>Finish</Center>
                 <BodyCenter>
-                    Please open{' '}
+                    You burned all NOT Vouchers. Notcoin will deposit to your wallet address
                     <TgLink
                         onClick={e => {
                             e.stopPropagation();
-                            sdk.openPage('https://t.me/notcoin_bot');
+                            sdk.openPage(`https://tonviewer.com/${wallet.active.friendlyAddress}`);
                         }}
                     >
-                        Notcoin Bot
-                    </TgLink>{' '}
-                    to index burning and get Notcoins!
+                        Check Tonviewer!
+                    </TgLink>
                 </BodyCenter>
             </NotFound>
         );
@@ -229,16 +356,7 @@ const BurnBlock: FC<{ data: NftItem[] | undefined }> = ({ data }) => {
                 </Center>
                 <BodyCenter>Please wait and don't close this page</BodyCenter>
                 <BodyCenter>
-                    Wallet should have enough TON for transfer NFTs. On finish please open{' '}
-                    <TgLink
-                        onClick={e => {
-                            e.stopPropagation();
-                            sdk.openPage('https://t.me/notcoin_bot');
-                        }}
-                    >
-                        Notcoin Bot
-                    </TgLink>{' '}
-                    to index burning.
+                    Wallet should have enough TON for transfer NFTs - 0.07 TON per voucher.
                 </BodyCenter>
             </NotFound>
         );
@@ -257,8 +375,18 @@ const BurnBlock: FC<{ data: NftItem[] | undefined }> = ({ data }) => {
                 {Math.round((Math.ceil(data.length / 4) * 30) / 60)} min
             </BodyCenter>
             <BodyCenter>
-                Please ensure that the wallet has sufficient TON to transfer NFTs. The wallet will
-                initiate multiple transactions to transfer the NFT vouchers to a Null Address.
+                Please ensure that the wallet has sufficient TON to transfer NFTs - 0.07 TON per
+                voucher. The wallet will initiate multiple transactions to transfer the NFT vouchers
+                to a Burning Smart Contracts. More Details{' '}
+                <TgLink
+                    onClick={e => {
+                        e.stopPropagation();
+                        sdk.openPage('https://t.me/notcoin_bot');
+                    }}
+                >
+                    Notcoin Bot
+                </TgLink>
+                .
             </BodyCenter>
             <Button primary onClick={onBurn}>
                 Burn NOT Vouchers
