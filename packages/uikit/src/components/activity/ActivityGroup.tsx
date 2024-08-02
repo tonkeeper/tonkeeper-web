15f9d58a45a0964e5c62b4d82cae1afa00ac2550
siandreev <andreev.sergey.i@yandex.ru>
2024-08-02 12:38:36 +0200

    fix: extension render props bug in history

--- packages/uikit/src/components/activity/ActivityGroup.tsx
+++ packages/uikit/src/components/activity/ActivityGroup.tsx
@@ -2,14 +2,15 @@ import { InfiniteData } from '@tanstack/react-query';
 import { AccountEvents } from '@tonkeeper/core/dist/tonApiV2';
 import { TronEvents } from '@tonkeeper/core/dist/tronApi';
 import React, { FC, useMemo, useState } from 'react';
-import { GenericActivityGroup } from '../../state/activity';
+import { formatActivityDate, GenericActivityGroup, getActivityTitle } from '../../state/activity';
 import { MixedActivity, getMixedActivityGroups } from '../../state/mixedActivity';
 import { CoinHistorySkeleton, HistoryBlock, SkeletonListWithImages } from '../Skeleton';
-import { ActivityBlock } from './ActivityLayout';
+import { Group, List, Title } from './ActivityLayout';
 import { ActionData, ActivityNotification } from './ton/ActivityNotification';
 import { TonActivityEvents } from './ton/TonActivityEvents';
 import { TronActionData, TronActivityNotification } from './tron/ActivityNotification';
 import { TronActivityEvents } from './tron/TronActivityEvents';
+import { useTranslation } from '../../hooks/translation';
 
 export const ActivityList: FC<{
     isFetched: boolean;
@@ -37,35 +38,41 @@ export const MixedActivityGroup: FC<{
 }> = ({ items }) => {
     const [tonAction, seTonAction] = useState<ActionData | undefined>(undefined);
     const [tronAction, setTronAction] = useState<TronActionData | undefined>(undefined);
+    const { i18n } = useTranslation();
 
     return (
         <>
-            <ActivityBlock
-                groups={items}
-                RenderItem={({ event, date, timestamp }) => {
-                    if (event.kind === 'tron') {
-                        return (
-                            <TronActivityEvents
-                                event={event.event}
-                                date={date}
-                                timestamp={timestamp}
-                                setTronAction={setTronAction}
-                            />
-                        );
-                    }
-                    if (event.kind === 'ton') {
-                        return (
-                            <TonActivityEvents
-                                event={event.event}
-                                date={date}
-                                timestamp={timestamp}
-                                setActivity={seTonAction}
-                            />
-                        );
-                    }
-                    return <></>;
-                }}
-            />
+            {items.map(([eventKey, events]) => {
+                return (
+                    <Group key={eventKey}>
+                        <Title>
+                            {getActivityTitle(i18n.language, eventKey, events[0].timestamp)}
+                        </Title>
+                        {events.map(({ timestamp, event, key }) => {
+                            const date = formatActivityDate(i18n.language, eventKey, timestamp);
+                            return (
+                                <List key={key}>
+                                    {event.kind === 'tron' ? (
+                                        <TronActivityEvents
+                                            event={event.event}
+                                            date={date}
+                                            timestamp={timestamp}
+                                            setTronAction={setTronAction}
+                                        />
+                                    ) : event.kind === 'ton' ? (
+                                        <TonActivityEvents
+                                            event={event.event}
+                                            date={date}
+                                            timestamp={timestamp}
+                                            setActivity={seTonAction}
+                                        />
+                                    ) : null}
+                                </List>
+                            );
+                        })}
+                    </Group>
+                );
+            })}
             <ActivityNotification value={tonAction} handleClose={() => seTonAction(undefined)} />
             <TronActivityNotification
                 value={tronAction}
