e285e4c4baaa8cc83e0bb9c6c03eb48c30380af3
siandreev <andreev.sergey.i@yandex.ru>
2024-08-08 18:50:27 +0200

    fix: apps directories types updates

--- packages/uikit/src/pages/import/ReadOnly.tsx
+++ packages/uikit/src/pages/import/ReadOnly.tsx
@@ -2,21 +2,14 @@ import { Account } from '@tonkeeper/core/dist/entries/account';
 import { useState } from 'react';
 import { AddressInput } from '../../components/create/AddressInput';
 import { UpdateWalletName } from '../../components/create/WalletName';
-import { useAppContext } from '../../hooks/appContext';
-import { useAppSdk } from '../../hooks/appSdk';
-import { useTranslation } from '../../hooks/translation';
 import { useCreateAccountReadOnly, useMutateRenameAccount } from '../../state/wallet';
 import { FinalView } from './Password';
 
 const ReadOnly = () => {
-    const sdk = useAppSdk();
-    const { t } = useTranslation();
-    const { defaultWalletVersion } = useAppContext();
     const { mutateAsync: createWalletsAsync, isLoading: isCreateWalletLoading } =
         useCreateAccountReadOnly();
     const { mutateAsync: renameWallet, isLoading: renameLoading } = useMutateRenameAccount();
 
-    const [address, setAddress] = useState<string | undefined>();
     const [createdAccount, setCreatedAccount] = useState<Account | undefined>(undefined);
 
     const [editNamePagePassed, setEditNamePagePassed] = useState(false);
