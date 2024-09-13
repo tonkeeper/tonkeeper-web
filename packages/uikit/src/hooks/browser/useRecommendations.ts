8361eb5439c4a421adc0e9932e01d9caaa1c8ed7
Nikita Kuznetsov <nkuz915@gmail.com>
2024-09-13 14:22:27 +0200

    shuffle apps

--- packages/uikit/src/hooks/browser/useRecommendations.ts
+++ packages/uikit/src/hooks/browser/useRecommendations.ts
@@ -4,6 +4,20 @@ import { QueryKey } from '../../libs/queryKey';
 import { useUserCountry } from '../../state/country';
 import { useAppContext } from '../appContext';
 
+function shuffle<T>(array: T[]) {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+}
+
 export function useRecommendations() {
     const { tonendpoint } = useAppContext();
     const country = useUserCountry();
@@ -14,6 +28,7 @@ export function useRecommendations() {
         // TODO: Remove mobile hack
         data.categories = data.categories.filter(item => item.id !== 'featured');
 
+        shuffle(data.apps);
         return data;
     });
 }
