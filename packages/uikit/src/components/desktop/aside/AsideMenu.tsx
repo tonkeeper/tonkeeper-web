5b85e63089194db363e84a42b23752d6ca7f0416
siandreev <andreev.sergey.i@yandex.ru>
2024-10-02 12:15:13 +0200

    fix(PRO-102): move discover to the top of the sidebar

--- packages/uikit/src/components/desktop/aside/AsideMenu.tsx
+++ packages/uikit/src/components/desktop/aside/AsideMenu.tsx
@@ -285,14 +285,6 @@ const AsideMenuPayload: FC<{ className?: string }> = ({ className }) => {
                             <Label2>{t('aside_dashboard')}</Label2>
                         </AsideMenuItem>
                     )}
-                    <AccountDNDBlock
-                        items={items}
-                        activeAccount={activeAccount}
-                        activeRoute={activeRoute}
-                    />
-                </ScrollContainer>
-                <AsideMenuBottom>
-                    <DividerStyled isHidden={!closeBottom} />
                     <AsideMenuItem
                         onClick={() => handleNavigateClick(AppRoute.browser)}
                         isSelected={activeRoute === AppRoute.browser}
@@ -302,6 +294,14 @@ const AsideMenuPayload: FC<{ className?: string }> = ({ className }) => {
                         </IconWrapper>
                         <Label2>{t('aside_discover')}</Label2>
                     </AsideMenuItem>
+                    <AccountDNDBlock
+                        items={items}
+                        activeAccount={activeAccount}
+                        activeRoute={activeRoute}
+                    />
+                </ScrollContainer>
+                <AsideMenuBottom>
+                    <DividerStyled isHidden={!closeBottom} />
                     <AsideMenuItem isSelected={false} onClick={() => addWallet()}>
                         <IconWrapper>
                             <PlusIcon />
