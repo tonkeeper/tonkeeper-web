f9ce6bcec51df2be103900d0ddc6067d8fa5be2f
siandreev <andreev.sergey.i@yandex.ru>
2024-11-05 21:47:49 +0400

    fix(PRO-119): BUG 4

--- packages/uikit/src/components/transfer/ConfirmTransferView.tsx
+++ packages/uikit/src/components/transfer/ConfirmTransferView.tsx
@@ -8,7 +8,32 @@ import {
     SenderTypeUserAvailable,
     useAvailableSendersChoices
 } from '../../hooks/blockchain/useSender';
-import { TonAsset } from '@tonkeeper/core/dist/entries/crypto/asset/ton-asset';
+import {
+    TonAsset,
+    tonAssetAddressToString
+} from '@tonkeeper/core/dist/entries/crypto/asset/ton-asset';
+import { TON_ASSET, TON_USDT_ASSET } from '@tonkeeper/core/dist/entries/crypto/asset/constants';
+import { useAssetWeiBalance } from '../../state/home';
+import { JettonEncoder } from '@tonkeeper/core/dist/service/ton-blockchain/encoder/jetton-encoder';
+import BigNumber from 'bignumber.js';
+import { RatesApi } from '@tonkeeper/core/dist/tonApiV2';
+import { useAppContext } from '../../hooks/appContext';
+import { isTonAsset } from '@tonkeeper/core/dist/entries/crypto/asset/asset';
+import { useQuery } from '@tanstack/react-query';
+import { shiftedDecimals } from '@tonkeeper/core/dist/utils/balance';
+
+const gaslessApproximateFee = (asset: TonAsset, tokenToTonRate: number) => {
+    const k = asset.id === TON_USDT_ASSET.id ? 0.9 : 0.5;
+
+    const relativeAmount = shiftedDecimals(
+        new BigNumber(JettonEncoder.jettonTransferAmount.toString())
+    )
+        .multipliedBy(1.2)
+        .div(k)
+        .div(tokenToTonRate);
+
+    return AssetAmount.fromRelativeAmount({ asset: asset, amount: relativeAmount });
+};
 
 export const ConfirmTransferView: FC<
     PropsWithChildren<{
@@ -19,13 +44,19 @@ export const ConfirmTransferView: FC<
         onClose: (confirmed?: boolean) => void;
         fitContent?: boolean;
     }>
-> = ({ isMax, ...rest }) => {
+> = ({ isMax, assetAmount, ...rest }) => {
+    const { api } = useAppContext();
     const operationType = useMemo(() => {
         return {
             type: 'transfer',
-            asset: rest.assetAmount.asset as TonAsset
+            asset: assetAmount.asset as TonAsset
         } as const;
-    }, [rest.assetAmount.asset]);
+    }, [assetAmount.asset]);
+    const assetWeiBalance = useAssetWeiBalance(assetAmount.asset);
+    /**
+     * for MAX button jettons gasless
+     */
+    const [assetAmountPatched, setAssetAmountPatched] = useState<AssetAmount>(assetAmount);
 
     const { data: availableSendersChoices } = useAvailableSendersChoices(operationType);
 
@@ -39,23 +70,70 @@ export const ConfirmTransferView: FC<
 
     const estimation = useEstimateTransfer({
         recipient: rest.recipient,
-        amount: rest.assetAmount,
+        amount: assetAmountPatched,
         isMax,
         senderType: selectedSenderType
     });
     const mutation = useSendTransfer({
         recipient: rest.recipient,
-        amount: rest.assetAmount,
+        amount: assetAmountPatched,
         isMax,
         estimation: estimation.data!,
         senderType: selectedSenderType!
     });
+
+    const assetAddress = isTonAsset(assetAmount.asset)
+        ? tonAssetAddressToString((assetAmount.asset as TonAsset).address)
+        : undefined;
+    const shouldPatchAmount =
+        assetAddress !== TON_ASSET.address &&
+        isMax &&
+        selectedSenderType === 'gasless' &&
+        isTonAsset(assetAmount.asset) &&
+        assetWeiBalance;
+
+    const tokenToTonRate = useQuery(
+        [
+            'current-token-to-ton-rate',
+            tonAssetAddressToString((assetAmount.asset as TonAsset).address)
+        ],
+        async () => {
+            const response = await new RatesApi(api.tonApiV2).getRates({
+                tokens: [assetAddress!],
+                currencies: ['TON']
+            });
+
+            return Object.values(response.rates)[0].prices!.TON;
+        },
+        {
+            enabled: !!shouldPatchAmount
+        }
+    );
+
+    useEffect(() => {
+        if (!shouldPatchAmount) {
+            return setAssetAmountPatched(assetAmount);
+        }
+
+        if (!tokenToTonRate.data) {
+            return;
+        }
+
+        const fee = gaslessApproximateFee(assetAmount.asset as TonAsset, tokenToTonRate.data);
+        setAssetAmountPatched(
+            new AssetAmount({
+                asset: assetAmount.asset,
+                weiAmount: assetWeiBalance.minus(fee.weiAmount)
+            })
+        );
+    }, [isMax, assetAmount, selectedSenderType, assetWeiBalance]);
 
     return (
         <ConfirmView
             estimation={estimation}
             {...mutation}
             {...rest}
+            assetAmount={assetAmountPatched}
             selectedSenderType={selectedSenderType}
             onSenderTypeChange={onSenderTypeChange}
             availableSendersChoices={availableSendersChoices}
