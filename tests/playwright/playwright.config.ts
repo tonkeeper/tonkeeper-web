7f40970f1ad11bef23e6873c888741f6ccb82a6c
Nikita Kuznetsov <nkuz915@gmail.com>
2024-11-18 20:41:55 +0100

    Keep chrome and ff

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -46,12 +46,12 @@ export default defineConfig({
         {
             name: 'firefox',
             use: { ...devices['Desktop Firefox'] }
-        },
+        }
 
-        {
-            name: 'webkit',
-            use: { ...devices['Desktop Safari'] }
-        },
+        // {
+        //     name: 'webkit',
+        //     use: { ...devices['Desktop Safari'] }
+        // },
 
         /* Test against mobile viewports. */
         // {
@@ -64,14 +64,14 @@ export default defineConfig({
         // },
 
         /* Test against branded browsers. */
-        {
-            name: 'Microsoft Edge',
-            use: { ...devices['Desktop Edge'], channel: 'msedge' }
-        },
-        {
-            name: 'Google Chrome',
-            use: { ...devices['Desktop Chrome'], channel: 'chrome' }
-        }
+        // {
+        //     name: 'Microsoft Edge',
+        //     use: { ...devices['Desktop Edge'], channel: 'msedge' }
+        // },
+        // {
+        //     name: 'Google Chrome',
+        //     use: { ...devices['Desktop Chrome'], channel: 'chrome' }
+        // }
     ]
 
     /* Run your local dev server before starting the tests */
