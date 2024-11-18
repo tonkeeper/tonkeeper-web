f61d161f5d7ab2be9f9bd039d52826f6b606f81c
Nikita Kuznetsov <nkuz915@gmail.com>
2024-11-18 23:09:30 +0100

    Fix negative test

--- tests/playwright/tests/Negative cases/Negative.spec.ts
+++ tests/playwright/tests/Negative cases/Negative.spec.ts
@@ -8,7 +8,7 @@ test('Incorrect seed phrase ', async ({ page }) => {
     await page.getByRole('button', { name: 'Get started' }).click();
     await page.getByRole('button', { name: 'Existing Wallet Import wallet' }).click();
     await page.getByLabel('1:', { exact: true }).click();
-    await page.getByLabel('1:', { exact: true }).fill(process.env.TON_MNEMONIC_ANANAS);
+    await page.getByLabel('1:', { exact: true }).fill(process.env.FIKE_MNEMONIC);
     await expect(page.getByRole('button', { name: 'Continue' })).toBeVisible();
     await expect(page.getByRole('button', { name: 'Continue' })).toBeVisible();
     await page.getByRole('button', { name: 'Continue' }).click();
