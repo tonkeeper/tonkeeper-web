c7432a849eda446c1b898df6531a9b734e81eec3
Nikita Kuznetsov <nkuz915@gmail.com>
2024-11-18 22:39:09 +0100

    Update retry and timeout

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 1 : 0,
+    retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 20 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -33,8 +33,8 @@ export default defineConfig({
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry'
     },
-    // Set timeout 1 min for each test
-    timeout: 60_000,
+    // Set timeout 30 sec for each test
+    timeout: 30 * 1000,
 
     /* Configure projects for major browsers */
     projects: [
