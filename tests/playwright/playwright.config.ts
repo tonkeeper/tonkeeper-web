3c475317f69214d7727f9831fd511e08a6ddb84a
Nikita Kuznetsov <nkuz915@gmail.com>
2024-11-18 19:30:53 +0100

    retries and workers

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -18,9 +18,9 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    retries: process.env.CI ? 1 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: process.env.CI ? 4 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: process.env.CI ? [['github'], ['list']] : [['html'], ['list']],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
