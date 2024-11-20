198b42629c497ae58ebceabd2c023aaf086088b2
siandreev <andreev.sergey.i@yandex.ru>
2024-11-20 11:24:23 +0100

    feat: 2fa api added

---
+++ packages/core/src/2faApi/models/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthRequest';
+export * from './Message';
+export * from './MessageID';
+export * from './MessageState';
+export * from './ModelError';
+export * from './Ok';
+export * from './Payload';
+export * from './PingReadyGet500Response';
+export * from './SendMessageRequest';
+export * from './Url';
