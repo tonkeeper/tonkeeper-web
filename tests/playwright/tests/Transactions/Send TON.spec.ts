e52462b552894dced2830d484d37dc0777c14418
tatsiosh <tatsiosh@tonkeeper.com>
2024-11-20 18:34:44 +0300

    Fix Send TON test & send USDT from token screen

--- tests/playwright/tests/Transactions/Send TON.spec.ts
+++ tests/playwright/tests/Transactions/Send TON.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+
+test.beforeEach(async ({ page }) => {
     await page.goto('/');
     await page.getByRole('button', { name: 'Get started' }).click();
     await page.getByRole('button', { name: 'Existing Wallet Import wallet' }).click();
@@ -18,23 +19,9 @@ test('test', async ({ page }) => {
     await page.getByRole('button', { name: 'Continue' }).click();
     await page.locator('#react-portal-modal-container').getByRole('textbox').fill('Ananas');
     await page.getByRole('button', { name: 'Save' }).click();
-    await page.getByRole('button', { name: 'Send', exact: true }).click();
-    await page.getByRole('textbox').first().click();
-    await page
-        .getByRole('textbox')
-        .first()
-        .fill('UQD2NmD_lH5f5u1Kj3KfGyTvhZSX0Eg6qp2a5IQUKXxOGzCi');
-    await page.getByRole('button', { name: 'Continue' }).click();
-    await page.getByLabel('TONTON0,00 USD').fill('0,01');
-    await page.getByText('TON', { exact: true }).nth(3).click();
-    await page.getByText('TONTON0,05 USD').click();
-    await page.getByText('TON', { exact: true }).nth(3).click();
-    await page.getByText('TONTON0,05 USD').click();
-    await page.getByRole('button', { name: 'Continue' }).click();
-    await page.getByRole('button', { name: 'Confirm and Send' }).click();
-    await page.locator('#react-portal-modal-container').getByRole('textbox').fill('123456');
-    await page.getByRole('button', { name: 'Confirm' }).click();
-    await page.locator('#react-portal-modal-container path').nth(2).click();
+});
+
+test.afterEach(async ({ page }) => {
     await page.getByRole('link', { name: 'Settings' }).click();
     await page.goto(`/wallet-settings`);
     await page.getByText('Delete Account').click();
@@ -44,4 +31,21 @@ test('test', async ({ page }) => {
         .locator('div')
         .click();
     await page.getByRole('button', { name: 'Delete wallet data' }).click();
+});
+
+test('Transfer TON', async ({ page }) => {
+    await page.getByRole('button', { name: 'Send', exact: true }).click();
+    await page.getByRole('textbox').first().click();
+    await page
+        .getByRole('textbox')
+        .first()
+        .fill('UQD2NmD_lH5f5u1Kj3KfGyTvhZSX0Eg6qp2a5IQUKXxOGzCi');
+    await page.getByRole('button', { name: 'Continue' }).click();
+    await page.getByTestId('amount-input').click();
+    await page.getByTestId('amount-input').fill('0.01');
+    await page.getByText('TON', { exact: true }).nth(3).click();
+    await page.getByRole('button', { name: 'Continue' }).click();
+    await page.getByRole('button', { name: 'Confirm and Send' }).click();
+    await page.locator('#react-portal-modal-container').getByRole('textbox').fill('123456');
+    await page.getByRole('button', { name: 'Confirm' }).click();
 });
