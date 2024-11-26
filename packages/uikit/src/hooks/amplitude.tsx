aaccd8a8d1f34edbf29e28a5c3dbdcf34b83bb6f
Nikita Kuznetsov <nkuz915@gmail.com>
2024-11-26 17:14:27 +0100

    Update event key

--- packages/uikit/src/hooks/amplitude.tsx
+++ packages/uikit/src/hooks/amplitude.tsx
@@ -98,9 +98,9 @@ export const useClickBrowser = () => {
     const tracker = useContext(AmplitudeAnalyticsContext);
 
     return useCallback(
-        (dapp: string, source: string) => {
+        (url: string, source: string) => {
             if (tracker) {
-                tracker.track('click_dapp', { dapp, source });
+                tracker.track('click_dapp', { url, source });
             }
         },
         [tracker]
