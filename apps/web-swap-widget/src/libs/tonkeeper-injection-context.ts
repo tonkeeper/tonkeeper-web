9424b099c166dd3b34b0d939d5fac9efd138276d
siandreev <andreev.sergey.i@yandex.ru>
2025-01-28 11:03:16 +0100

    fix: emulation link

--- apps/web-swap-widget/src/libs/tonkeeper-injection-context.ts
+++ apps/web-swap-widget/src/libs/tonkeeper-injection-context.ts
@@ -3,6 +3,7 @@ import {
     TON_CONNECT_MSG_VARIANTS_ID,
     TonConnectTransactionPayloadMessage
 } from '@tonkeeper/core/dist/entries/tonConnect';
+import { Address, beginCell, Cell, storeMessage } from '@ton/core';
 
 type UserFriendlyAddress = string;
 type TimestampMS = number;
@@ -74,6 +75,32 @@ export const provideMockInjectionContext = () => {
                     msg.amount
                 }&bin=${encodeURIComponent(msg.payload!)}`
             );
+
+            const cell = beginCell()
+                .store(
+                    storeMessage({
+                        info: {
+                            type: 'internal',
+                            ihrDisabled: true,
+                            bounce: false,
+                            bounced: false,
+                            src: Address.parse('UQBAA8u_tdGpCFhz-Vb5IEBmmZDZ03HOhDdBtdeTbyMSOP8V'),
+                            dest: Address.parse(msg.address),
+                            value: { coins: BigInt(msg.amount) },
+                            ihrFee: 0n,
+                            forwardFee: 0n,
+                            createdLt: 0n,
+                            createdAt: 0
+                        },
+                        body: Cell.fromBase64(msg.payload!)
+                    })
+                )
+                .endCell()
+                .toBoc()
+                .toString('hex');
+
+            console.log(`https://tonviewer.com/emulate/${cell}`);
+
             return 'mock_success_boc';
         }
     };
