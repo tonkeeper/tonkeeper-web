cd2857b304bbd47112009037e0c6cfac1322bc91
siandreev <andreev.sergey.i@yandex.ru>
2025-02-14 12:31:25 +0700

    fix: await deploy multisig

--- packages/uikit/src/hooks/blockchain/multisig/useDeployMultisig.ts
+++ packages/uikit/src/hooks/blockchain/multisig/useDeployMultisig.ts
@@ -11,7 +11,7 @@ import { useCheckTouchId } from '../../../state/password';
 import { useTranslation } from '../../translation';
 import { anyOfKeysParts, QueryKey } from '../../../libs/queryKey';
 import BigNumber from 'bignumber.js';
-import { AccountsApi, Multisig } from '@tonkeeper/core/dist/tonApiV2';
+import { AccountsApi, Multisig, MultisigApi } from '@tonkeeper/core/dist/tonApiV2';
 import { useAccountsStorage } from '../../useStorage';
 import { TxConfirmationCustomError } from '../../../libs/errors/TxConfirmationCustomError';
 import {
@@ -153,10 +153,19 @@ const checkIfMultisigExists = async (options: { api: APIConfig; address: Address
 export const useAwaitMultisigIsDeployed = () => {
     const client = useQueryClient();
     const accounts = useAccountsStorage();
+    const api = useActiveApi();
+
     return useMutation<void, Error, { multisigAddress: string; deployerWalletId: WalletId }>(
         async ({ multisigAddress, deployerWalletId }) => {
             const awaitIsDeployed = async (attempt = 0): Promise<void> => {
                 try {
+                    await client.prefetchQuery(
+                        [QueryKey.multisigWallet, multisigAddress],
+                        async () =>
+                            new MultisigApi(api.tonApiV2).getMultisigAccount({
+                                accountId: multisigAddress
+                            })
+                    );
                     await client.refetchQueries([QueryKey.multisigWallet, multisigAddress]);
                     const deployed = client.getQueryData<Multisig>([
                         QueryKey.multisigWallet,
