4a34074505dd1b681c9284cf2595961086e57eaf
Nikita Kuznetsov <nkuz915@gmail.com>
2025-02-25 15:40:37 +0100

    Update navigation

--- packages/uikit/src/state/two-fa.ts
+++ packages/uikit/src/state/two-fa.ts
@@ -16,7 +16,7 @@ import { useEffect, useMemo } from 'react';
 import { useSignTonProof } from '../hooks/accountUtils';
 import { TwoFAEncoder } from '@tonkeeper/core/dist/service/ton-blockchain/encoder/two-fa-encoder';
 import { assertUnreachable } from '@tonkeeper/core/dist/utils/types';
-import { WalletId } from '@tonkeeper/core/dist/entries/wallet';
+import { TonWalletStandard, WalletId, WalletVersion } from '@tonkeeper/core/dist/entries/wallet';
 import { useToast } from '../hooks/useNotification';
 import { useTranslation } from '../hooks/translation';
 import { getMultisigSignerInfo } from './multisig';
@@ -71,7 +71,9 @@ export const useCanViewTwoFA = () => {
 
     const isSuitableAccount = account.type === 'mnemonic' || account.type === 'mam';
 
-    return (isEnabled && isSuitableAccount) || data?.status === 'active';
+    const isW5 =
+        (account.activeTonWallet as Partial<TonWalletStandard>)?.version === WalletVersion.V5R1;
+    return (isEnabled && isSuitableAccount && isW5) || data?.status === 'active';
 };
 
 export const useTwoFAApi = () => {
@@ -132,16 +134,18 @@ export const useTwoFAWalletConfig = (options?: { account?: Account; walletId?: W
     const activeAccount = useActiveAccount();
     const account = options?.account ?? activeAccount;
 
-    const wallet = options?.walletId
-        ? account.getTonWallet(options.walletId)!
-        : account.activeTonWallet;
+    const wallet = (
+        options?.walletId ? account.getTonWallet(options.walletId)! : account.activeTonWallet
+    ) as TonWalletStandard;
+
     const twoFAApi = useTwoFAApi();
     const api = useActiveApi();
 
     const isSuitableAccount = account.type === 'mnemonic' || account.type === 'mam';
     const serviceConfig = useTwoFAServiceConfig();
 
-    const isEnabled = isSuitableAccount;
+    const isEnabled = isSuitableAccount && wallet.version == WalletVersion.V5R1;
+
     const query = useQuery<TwoFAWalletConfig>(
         [QueryKey.twoFAWalletConfig, wallet.id],
         async () => {
@@ -215,7 +219,7 @@ export const useTwoFAWalletConfig = (options?: { account?: Account; walletId?: W
             return config;
         },
         {
-            keepPreviousData: true,
+            //  keepPreviousData: true,
             enabled: isEnabled,
             refetchInterval: d =>
                 d?.status === 'tg-bot-bounding' || d?.status === 'ready-for-deployment'
