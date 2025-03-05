e7ba06f96366dc0bb7506449fc104a4d161df164
Nikita Kuznetsov <nkuz915@gmail.com>
2025-03-05 18:57:12 +0100

    Fix multichain wallet label

--- packages/uikit/src/components/desktop/aside/AsideHeaderWallet.tsx
+++ packages/uikit/src/components/desktop/aside/AsideHeaderWallet.tsx
@@ -10,7 +10,7 @@ import { ChevronDownIcon, CopyIcon, DoneIcon } from '../../Icon';
 import { Transition } from 'react-transition-group';
 import { AccountAndWalletBadgesGroup } from '../../account/AccountBadge';
 import { AsideHeaderContainer } from './AsideHeaderElements';
-import { useActiveTronWallet } from '../../../state/tron/tron';
+import { useActiveTronWallet, useIsTronEnabledForActiveWallet } from '../../../state/tron/tron';
 import { DropDownContent, DropDownItem, DropDownItemsDivider } from '../../DropDown';
 import { SelectDropDown } from '../../fields/Select';
 import { AccountMAM, AccountTonMnemonic } from '@tonkeeper/core/dist/entries/account';
@@ -63,9 +63,9 @@ const DoneIconStyled = styled(DoneIcon)`
 `;
 
 export const AsideHeaderWallet: FC<{ width: number }> = ({ width }) => {
-    const tronWallet = useActiveTronWallet();
+    const isTronEnabled = useIsTronEnabledForActiveWallet();
 
-    if (!tronWallet) {
+    if (!isTronEnabled) {
         return <AsideHeaderSingleChainWallet width={width} />;
     }
 
