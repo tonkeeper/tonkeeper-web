0ddb33a770bae17a5820d8550fbf5a42bab21833
siandreev <andreev.sergey.i@yandex.ru>
2025-03-05 20:47:08 +0400

    fix(PRO-180): BUG 6

--- packages/uikit/src/components/settings/AccountSettings.tsx
+++ packages/uikit/src/components/settings/AccountSettings.tsx
@@ -20,7 +20,8 @@ import {
     SettingsProIcon,
     WalletsIcon,
     BatteryIcon,
-    LockIcon
+    LockIcon,
+    BlockchainsIcon
 } from './SettingsIcons';
 import { SettingsItem, SettingsList } from './SettingsList';
 import {
@@ -30,6 +31,7 @@ import {
 } from '@tonkeeper/core/dist/entries/account';
 import { useBatteryEnabledConfig } from '../../state/battery';
 import { useCanViewTwoFA } from '../../state/two-fa';
+import { useCanUseTronForActiveWallet } from '../../state/tron/tron';
 
 const SingleAccountSettings = () => {
     const { t } = useTranslation();
@@ -41,6 +43,7 @@ const SingleAccountSettings = () => {
     const { onOpen: rename } = useRenameNotification();
     const batteryEnableConfig = useBatteryEnabledConfig();
     const twoFAEnabled = useCanViewTwoFA();
+    const canUseTron = useCanUseTronForActiveWallet();
 
     const mainItems = useMemo<SettingsItem[]>(() => {
         const items: SettingsItem[] = [];
@@ -160,6 +163,14 @@ const SingleAccountSettings = () => {
             });
         }
 
+        if (canUseTron) {
+            items.push({
+                name: t('chains_title'),
+                icon: <BlockchainsIcon />,
+                action: () => navigate(relative(WalletSettingsRoute.chains))
+            });
+        }
+
         return items;
     }, [t, navigate, account, jettons, nft, twoFAEnabled, batteryEnableConfig]);
 
@@ -185,6 +196,8 @@ const MultipleAccountSettings = () => {
     const [deleteAccount, setDeleteAccount] = useState(false);
 
     const wallet = account.activeTonWallet;
+
+    const canUseTron = useCanUseTronForActiveWallet();
 
     const accountItems = useMemo(() => {
         const items: SettingsItem[] = [
@@ -320,6 +333,14 @@ const MultipleAccountSettings = () => {
                 name: t('battery_title'),
                 icon: <BatteryIcon />,
                 action: () => navigate(relative(WalletSettingsRoute.battery))
+            });
+        }
+
+        if (canUseTron) {
+            items.push({
+                name: t('chains_title'),
+                icon: <BlockchainsIcon />,
+                action: () => navigate(relative(WalletSettingsRoute.chains))
             });
         }
 
