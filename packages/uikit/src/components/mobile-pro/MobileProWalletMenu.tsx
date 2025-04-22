cc328a1b67bd9782c57f7b640c8971296ea2c70a
siandreev <andreev.sergey.i@yandex.ru>
2025-04-22 15:42:19 +0200

    fix(PRO-227): BUG 12

--- packages/uikit/src/components/mobile-pro/MobileProWalletMenu.tsx
+++ packages/uikit/src/components/mobile-pro/MobileProWalletMenu.tsx
@@ -2,13 +2,23 @@ import { WalletAsideMenu } from '../desktop/aside/WalletAsideMenu';
 import { IonMenu } from '@ionic/react';
 import { atom } from '@tonkeeper/core/dist/entries/atom';
 import { useAtom } from '../../libs/useAtom';
+import { AppProRoute, AppRoute } from '../../libs/routes';
+import { useLocation } from 'react-router-dom';
+import { useMemo } from 'react';
 
 const isProWalletMenuOpen$ = atom(false);
 
 export const useIsProWalletMenuOpened = () => useAtom(isProWalletMenuOpen$);
 
+const disableMenuForRoutes = [AppRoute.browser, AppProRoute.dashboard];
+
 export const MobileProWalletMenu = () => {
     const [_, setIsOpen] = useIsProWalletMenuOpened();
+    const location = useLocation();
+    const shouldDisableMenu = useMemo(
+        () => disableMenuForRoutes.some(path => location.pathname.startsWith(path)),
+        [location.pathname]
+    );
 
     return (
         <IonMenu
@@ -18,6 +28,8 @@ export const MobileProWalletMenu = () => {
             type="overlay"
             onIonDidClose={() => setIsOpen(false)}
             onIonWillOpen={() => setIsOpen(true)}
+            swipeGesture
+            disabled={shouldDisableMenu}
         >
             <WalletAsideMenu />
         </IonMenu>
