42cf12eedbbb2e27b8cfc5db9e08df3a24a30ba1
siandreev <andreev.sergey.i@yandex.ru>
2025-05-01 12:56:11 +0200

    fix: disable scanner temporary

--- apps/twa/src/components/TwaQrScanner.tsx
+++ apps/twa/src/components/TwaQrScanner.tsx
@@ -1,26 +1,12 @@
-import { useQRScanner } from '@tma.js/sdk-react';
 import { useAppSdk } from '@tonkeeper/uikit/dist/hooks/appSdk';
 import { useEffect } from 'react';
 
 export const TwaQrScanner = () => {
-    const scanner = useQRScanner();
-
     const sdk = useAppSdk();
 
     useEffect(() => {
         const handler = async (options: { method: 'scan'; id?: number | undefined }) => {
-            if (scanner.isOpened) {
-                scanner.close();
-            }
-            const signature = await scanner.open();
-            if (signature) {
-                sdk.uiEvents.emit('response', {
-                    method: 'response',
-                    id: options.id,
-                    params: signature
-                });
-            }
-            scanner.close();
+            alert('Scanning is temporarily unavailable');
         };
         sdk.uiEvents.on('scan', handler);
         return () => {
