0a452d8d9f767afa632b52a83bf0fa10a56f7967
Dmitrii Liulekin <liulekin@tonkeeper.com>
2025-07-02 15:53:29 -0300

    fix: change network settings

--- packages/core/src/entries/network.ts
+++ packages/core/src/entries/network.ts
@@ -1,5 +1,5 @@
 import { Configuration as ConfigurationV2 } from '../tonApiV2';
-import { OpenAPI as TonConsoleApi } from '../tonConsoleApi';
+import { OpenAPI as TonConsoleApi } from '../pro';
 import { TonendpointConfig } from '../tonkeeperApi/tonendpoint';
 
 export enum Network {
@@ -26,8 +26,10 @@ export const getTonClientV2 = (config: TonendpointConfig, current?: Network) => 
 export const getApiConfig = (config: TonendpointConfig, network: Network, TonConsoleBase = '') => {
     // Global config
     if (TonConsoleBase) {
-        TonConsoleApi.BASE = TonConsoleBase;
-        TonConsoleApi.WITH_CREDENTIALS = true;
+        // TODO Find out why
+        TonConsoleApi.BASE = 'https://dev-pro.tonconsole.com';
+        TonConsoleApi.WITH_CREDENTIALS = false;
+        TonConsoleApi.CREDENTIALS = 'omit';
     }
 
     return {
