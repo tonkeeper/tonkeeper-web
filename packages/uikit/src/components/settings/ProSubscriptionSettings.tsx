8d797a6878612f761beecde1e5ed87e7f72f92e1
Dmitrii Liulekin <liulekin@tonkeeper.com>
2025-07-01 21:33:38 -0300

    fix: update common state of ProSubscriptionSettings

--- packages/uikit/src/components/settings/ProSubscriptionSettings.tsx
+++ packages/uikit/src/components/settings/ProSubscriptionSettings.tsx
@@ -1,4 +1,4 @@
-import { type FC } from 'react';
+import { type FC, useEffect } from 'react';
 import styled from 'styled-components';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
@@ -12,6 +12,7 @@ import {
 import { SubHeader } from '../SubHeader';
 import { AppRoute } from '../../libs/routes';
 import { duration } from '../transfer/common';
+import { useProState } from '../../state/pro';
 import { HideOnReview } from '../ios/HideOnReview';
 import { isDirectionForward } from '../../libs/pro';
 import { SubscriptionScreens } from '../../enums/pro';
@@ -24,6 +25,7 @@ import { ProPurchaseChooseScreen } from '../pro/ProPurchaseChooseScreen';
 import { useSubscriptionScreen } from '../../hooks/pro/useSubscriptionScreen';
 import { SubscriptionFlowProvider } from '../../providers/SubscriptionFlowProvider';
 import { leftToTight, rightToLeft, SlideAnimation } from '../shared/SlideAnimation';
+import { useGoToSubscriptionScreen } from '../../hooks/pro/useGoToSubscriptionScreen';
 
 const SCREENS_MAP = {
     [SubscriptionScreens.ACCOUNTS]: <ProAccountChooseScreen />,
@@ -33,7 +35,15 @@ const SCREENS_MAP = {
 
 export const ProSettingsContent: FC = () => {
     const { currentScreen, prevScreen } = useSubscriptionScreen();
+    const goTo = useGoToSubscriptionScreen();
     const navigate = useNavigate();
+    const { data: proState } = useProState();
+
+    useEffect(() => {
+        if (proState?.subscription?.valid) {
+            goTo(SubscriptionScreens.STATUS);
+        }
+    }, [proState]);
 
     const handleCloseClick = () => {
         navigate(AppRoute.home, { replace: true });
@@ -108,4 +118,6 @@ const AnimatedScreensWrapper = styled(SlideAnimation)`
     padding-top: env(safe-area-inset-top);
     box-sizing: border-box;
     height: 100%;
+    max-width: 768px;
+    margin: 0 auto;
 `;
