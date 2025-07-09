c51203fe2c8d6c94c20f1f5b5cd060f4ee6d86b5
tatsiosh <tatsiosh@tonkeeper.com>
2025-07-09 13:14:20 +0300

    Update 2 wallets & bulk delete.spec.ts

--- tests/playwright/tests/Add2WalletsAndDeleteInABulk/2 wallets & bulk delete.spec.ts
+++ tests/playwright/tests/Add2WalletsAndDeleteInABulk/2 wallets & bulk delete.spec.ts
@@ -11,22 +11,36 @@ test('2 wallets & bulk delete', async ({ page }) => {
     await page.locator('#create-password').fill('123456');
     await page.locator('#create-password-confirm').fill('123456');
     await page.getByRole('button', { name: 'Continue' }).click();
-    await page.getByRole('button', { name: 'Save' }).click();
+    await page.getByRole('button', { name: 'Continue' }).click();
+    await expect(page.getByRole('heading', { name: 'Wallet Tokens Setup' })).toBeVisible();
+    await page.getByText('Configure token support for').click();
+    await expect(page.locator('form')).toContainText('Configure token support for easier wallet management.');
+    await page.getByText('USD₮TRC20').click();
+    await page.getByText('Use USD₮ TRC20 without TRX.').click();
+    await page.getByRole('button', { name: 'Continue' }).click();
     await page.getByText('Add Wallet').click();
     await page.getByRole('button', { name: 'Existing Wallet Import wallet' }).click();
-    await page.getByLabel('1:', { exact: true }).click();
-    await page.getByLabel('1:', { exact: true }).fill(process.env.TON_MNEMONIC_ANANAS);
+    await page.locator('#react-portal-modal-container').getByRole('textbox').fill('123456');
+    await page.getByRole('button', { name: 'Confirm' }).click();
+    await page.getByLabel('1:', { exact: true }).fill(process.env.TON_MNEMONIC_24_4);
     await page.getByRole('button', { name: 'Continue' }).click();
     await page.getByRole('button', { name: 'Continue' }).click();
     await page.locator('#react-portal-modal-container').getByRole('textbox').fill('123456');
     await page.getByRole('button', { name: 'Confirm' }).click();
-    await page.getByRole('button', { name: 'Save' }).click();
+    await page.getByRole('button', { name: 'Continue' }).click();
+    await expect(page.getByRole('heading', { name: 'Wallet Tokens Setup' })).toBeVisible();
+    await page.getByText('Configure token support for').click();
+    await expect(page.locator('form')).toContainText('Configure token support for easier wallet management.');
+    await page.getByText('USD₮TRC20').click();
+    await page.getByText('Use USD₮ TRC20 without TRX.').click();
+    await page.getByRole('button', { name: 'Continue' }).click();
     await page.getByText('Preferences').click();
     await page.getByText('Sign Out of all Accounts').click();
     await page
         .locator('div')
         .filter({ hasText: /^I have a backup copy of recovery phrase$/ })
         .locator('div')
-        .click();
+    await page.locator('.sc-dwcvcB').click();
     await page.getByRole('button', { name: 'Delete wallet data' }).click();
+    await expect(page.getByRole('button', { name: 'Get started' })).toBeVisible();
 });
