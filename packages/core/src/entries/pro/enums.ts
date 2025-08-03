0b704a30aa62c0eea6c8be1ac9ba17801f9d55a6
Dmitrii Liulekin <liulekin@tonkeeper.com>
2025-08-03 18:03:41 -0300

    fix: separate interfaces

---
+++ packages/core/src/entries/pro/enums.ts
@@ -0,0 +1,34 @@
+export enum PurchaseStatuses {
+    SUCCESS = 'success',
+    PENDING = 'pending',
+    CANCELED = 'cancelled'
+}
+
+export enum IosSubscriptionStatuses {
+    ACTIVE = 'active',
+    EXPIRED = 'expired'
+}
+
+export enum ProductIds {
+    MONTHLY = 'com.tonapps.tonkeeperpro.subscription.pro.monthly'
+}
+
+export enum IosEnvironmentTypes {
+    SANDBOX = 'Sandbox'
+}
+
+export enum AuthTypes {
+    WALLET = 'wallet',
+    TELEGRAM = 'telegram'
+}
+
+export enum CryptoSubscriptionStatuses {
+    ACTIVE = 'active',
+    EXPIRED = 'expired',
+    PENDING = 'pending'
+}
+
+export enum TelegramSubscriptionStatuses {
+    ACTIVE = 'active',
+    EXPIRED = 'expired'
+}
