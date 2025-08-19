a3fce50885c15cdea95515ed50d772ccaaeab257
siandreev <andreev.sergey.i@yandex.ru>
2025-08-19 13:43:15 +0200

    fix: enable scanner

--- apps/twa/src/components/TwaQrScanner.tsx
+++ apps/twa/src/components/TwaQrScanner.tsx
@@ -1,18 +1,32 @@
+import { useQRScanner } from '@tma.js/sdk-react';
 import { useAppSdk } from '@tonkeeper/uikit/dist/hooks/appSdk';
 import { useEffect } from 'react';
 
 export const TwaQrScanner = () => {
-    const sdk = useAppSdk();
+  const scanner = useQRScanner();
 
-    useEffect(() => {
-        const handler = async (options: { method: 'scan'; id?: number | undefined }) => {
-            alert('Scanning is temporarily unavailable');
-        };
-        sdk.uiEvents.on('scan', handler);
-        return () => {
-            sdk.uiEvents.off('scan', handler);
-        };
-    }, []);
+  const sdk = useAppSdk();
 
-    return <></>;
+  useEffect(() => {
+    const handler = async (options: { method: 'scan'; id?: number | undefined }) => {
+      if (scanner.isOpened) {
+        scanner.close();
+      }
+      const signature = await scanner.open();
+      if (signature) {
+        sdk.uiEvents.emit('response', {
+          method: 'response',
+          id: options.id,
+          params: signature
+        });
+      }
+      scanner.close();
+    };
+    sdk.uiEvents.on('scan', handler);
+    return () => {
+      sdk.uiEvents.off('scan', handler);
+    };
+  }, []);
+
+  return <></>;
 };
