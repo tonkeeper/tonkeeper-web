8b81d3fe9e704107625df6a1be0eee404ecfa0ce
Dmitrii Liulekin <liulekin@tonkeeper.com>
2025-09-04 21:25:35 -0400

    chore: update backend

---
+++ packages/core/src/pro/models/SubscriptionExtensionStatus.ts
@@ -0,0 +1,10 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export enum SubscriptionExtensionStatus {
+    NOT_INITIALIZED = 'not_initialized',
+    ACTIVE = 'active',
+    CANCELLED = 'cancelled',
+    INVALID = 'invalid',
+}
