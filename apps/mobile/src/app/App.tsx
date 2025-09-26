b5790c5ccb5ed6cf39a111beab75b3a91641922d
siandreev <andreev.sergey.i@yandex.ru>
2025-09-26 13:31:23 +0300

    fix: log ios startup time

--- apps/mobile/src/app/App.tsx
+++ apps/mobile/src/app/App.tsx
@@ -129,6 +129,10 @@ export const Providers = () => {
         document.body.classList.add(CAPACITOR_APPLICATION_ID);
     }, []);
 
+    useEffect(() => {
+        console.log(new Date(), 'providers mounted');
+    }, []);
+
     return (
         <QueryClientProvider client={queryClient}>
             <Suspense fallback={<div></div>}>
@@ -145,6 +149,9 @@ export const Providers = () => {
 };
 
 export const App = () => {
+    useEffect(() => {
+        console.log(new Date(), 'app mounted');
+    }, []);
     return (
         <IonApp>
             <IonReactRouter>
@@ -224,6 +231,10 @@ export const Loader: FC = () => {
         }
     }, [config]);
 
+    useEffect(() => {
+        console.log(new Date(), 'Loader mounted');
+    }, []);
+
     if (
         activeWalletLoading ||
         isLangLoading ||
@@ -275,6 +286,9 @@ const Content: FC<{
     activeAccount?: Account | null;
     lock: boolean;
 }> = props => {
+    useEffect(() => {
+        console.log(new Date(), 'Content mounted');
+    }, []);
     if (CAPACITOR_APPLICATION_ID === 'mobile') {
         return <NarrowContent {...props} />;
     }
