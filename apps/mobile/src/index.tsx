b277bd44cd7374e7e5f8e4c16d98ceff0d2eb9e2
siandreev <andreev.sergey.i@yandex.ru>
2025-10-06 13:38:11 +0300

    fix: remove console.log from main

--- apps/mobile/src/index.tsx
+++ apps/mobile/src/index.tsx
@@ -4,7 +4,6 @@ import { setupLogger } from './libs/logger';
 async function main() {
     try {
         setupLogger();
-        console.log('TEST Starting storage migration from Preferences to Keychain...');
         await migrateCapacitorStorage();
     } catch (e) {
         console.error(e);
