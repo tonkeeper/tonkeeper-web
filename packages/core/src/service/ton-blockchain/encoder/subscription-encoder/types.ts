d89193a3e66028acf2405f6e2c955b0c9aa3239a
Dmitrii Liulekin <liulekin@tonkeeper.com>
2025-10-06 21:16:12 -0300

    fix: create prepareSubscriptionParamsForEncoder

--- packages/core/src/service/ton-blockchain/encoder/subscription-encoder/types.ts
+++ packages/core/src/service/ton-blockchain/encoder/subscription-encoder/types.ts
@@ -1,7 +1,10 @@
 import { Address, Builder, Cell, MessageRelaxed } from '@ton/core';
 import { StateInit as TonStateInit } from '@ton/core/dist/types/StateInit';
+
+import { TonWalletStandard } from '../../../../entries/wallet';
 import { OutActionWalletV4 } from '@ton/ton/dist/wallets/WalletContractV4';
 import { OutActionWalletV5 } from '@ton/ton/dist/wallets/v5beta/WalletV5OutActions';
+import { SubscriptionExtension, SubscriptionExtensionMetadata } from '../../../../pro';
 
 export type EncodedSubscriptionResult = OutActionWalletV5[] | OutActionWalletV4;
 
@@ -15,3 +18,22 @@ export interface IPayloadEncoder {
     getInternalFromAction(outgoingMsg: EncodedSubscriptionResult, to?: Address): MessageRelaxed;
     encodeDestructAction(extension: Address, destroyValue: bigint): EncodedSubscriptionResult;
 }
+
+export type DeployParams = {
+    beneficiary: Address;
+    subscriptionId?: number;
+    firstChargingDate: number;
+    paymentPerPeriod: bigint;
+    period: number;
+    gracePeriod: number;
+    callerFee: bigint;
+    deployValue: bigint;
+    withdrawAddress: Address;
+    withdrawMsgBody?: string;
+    metadata?: SubscriptionExtensionMetadata;
+    walletMetaKeyPair: nacl.SignKeyPair;
+};
+
+export type SubscriptionEncodingParams = {
+    selectedWallet: TonWalletStandard;
+} & SubscriptionExtension;
